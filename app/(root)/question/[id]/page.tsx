c15ce36546fbf769a4af8ca6a21f10bd132f6135
OTMinion <vietsherlock123@gmail.com>
2023-11-26 00:46:59 +0000

    done

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -1,14 +1,26 @@
 import Answer from "@/components/forms/Answer";
+import AllAnswers from "@/components/shared/AllAnswers";
 import Metric from "@/components/shared/Metric";
 import ParseHTML from "@/components/shared/ParseHTML";
 import RenderTag from "@/components/shared/RenderTag";
+import Votes from "@/components/shared/Votes";
 import { getQuestionById } from "@/lib/actions/question.action";
+import { getUserById } from "@/lib/actions/user.action";
 import { formatAndDivideNumber, getTimestamp } from "@/lib/utils";
+import { auth } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 const Page = async ({ params, searchParams }) => {
+  const { userId: clerkId } = auth();
+
+  let mongoUser;
+
+  if (clerkId) {
+    mongoUser = await getUserById({ userId: clerkId });
+  }
+
   const result = await getQuestionById({ questionId: params.id });
 
   return (
@@ -18,15 +30,16 @@ const Page = async ({ params, searchParams }) => {
           <Link href={`/profile/${result.author.clerkId}`} className="flex items-center justify-start gap-1">
             <Image
               src={result.author.picture}
-              alt="profile"
               className="rounded-full"
               width={22}
               height={22}
+              alt="profile"
             />
             <p className="paragraph-semibold text-dark300_light700">{result.author.name}</p>
           </Link>
-
-          <div className="flex justify-end">Voting</div>
+          <div className="flex justify-end">
+            <Votes />
+          </div>
         </div>
         <h2 className="h2-semibold text-dark200_light900 mt-3.5 w-full text-left">{result.title}</h2>
       </div>
@@ -63,7 +76,17 @@ const Page = async ({ params, searchParams }) => {
         ))}
       </div>
 
-      <Answer />
+      <AllAnswers
+        questionId={result._id}
+        userId={JSON.stringify(mongoUser._id)}
+        totalAnswers={result.answers.length}
+      />
+
+      <Answer
+        question={result.content}
+        questionId={JSON.stringify(result._id)}
+        authorId={JSON.stringify(mongoUser._id)}
+      />
     </>
   );
 };
---
+++ lib/actions/answer.action.ts
@@ -0,0 +1,50 @@
+"use server";
+
+import Question from "@/database/question.model";
+import { connectToDatabase } from "../mongoose";
+import { CreateAnswerParams, GetAnswersParams } from "./shared.types";
+import { revalidatePath } from "next/cache";
+import Answer from "@/database/answer.model";
+
+export async function createAnswer(params: CreateAnswerParams) {
+  try {
+    connectToDatabase();
+
+    // gets the values from the front end
+    const { content, author, question, path } = params;
+
+    // create a new answer
+    const newAnswer = await Answer.create({
+      content,
+      author,
+      question,
+    });
+
+    // add answer to questions's answer array
+    await Question.findByIdAndUpdate(question, {
+      $push: { answers: newAnswer._id },
+    });
+
+    revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getAnswers(params: GetAnswersParams) {
+  try {
+    connectToDatabase();
+
+    const { questionId } = params;
+
+    const answers = await Answer.find({ question: questionId })
+      .populate("author", "_id clerkId name picture")
+      .sort({ createdAt: -1 });
+
+    return { answers };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
