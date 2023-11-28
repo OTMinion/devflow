597a3699c2f36f8c01e9d71aa5946acb78b2c7eb
OTMinion <vietsherlock123@gmail.com>
2023-11-28 08:17:22 +0000

    done

--- components/shared/Votes.tsx
+++ components/shared/Votes.tsx
@@ -1,11 +1,13 @@
 "use client";
+
 import { downvoteAnswer, upvoteAnswer } from "@/lib/actions/answer.action";
+import { viewQuestion } from "@/lib/actions/interaction.action";
 import { downvoteQuestion, upvoteQuestion } from "@/lib/actions/question.action";
 import { toggleSaveQuestion } from "@/lib/actions/user.action";
 import { formatAndDivideNumber } from "@/lib/utils";
 import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
-import React from "react";
+import { useEffect } from "react";
 
 interface Props {
   type: string;
@@ -54,6 +56,7 @@ const Votes = ({ type, itemId, userId, upvotes, hasupVoted, downvotes, hasdownVo
         });
       }
 
+      // todo: show a toast
       return;
     }
 
@@ -75,8 +78,17 @@ const Votes = ({ type, itemId, userId, upvotes, hasupVoted, downvotes, hasdownVo
           path: pathname,
         });
       }
+
+      // todo: show a toast
     }
   };
+
+  useEffect(() => {
+    viewQuestion({
+      questionId: JSON.parse(itemId),
+      userId: userId ? JSON.parse(userId) : undefined,
+    });
+  }, [itemId, userId, pathname, router]);
 
   return (
     <div className="flex gap-5">
@@ -84,16 +96,16 @@ const Votes = ({ type, itemId, userId, upvotes, hasupVoted, downvotes, hasdownVo
         <div className="flex-center gap-1.5">
           <Image
             src={hasupVoted ? "/assets/icons/upvoted.svg" : "/assets/icons/upvote.svg"}
-            alt="upvote"
-            className="cursor-pointer"
             width={18}
             height={18}
+            alt="upvote"
+            className="cursor-pointer"
             onClick={() => handleVote("upvote")}
           />
-        </div>
 
-        <div className="flex-center background-light700_dark400 min-w-[18px] rounded-sm p-1">
-          <p className="subtle-medium text-dark400_light900">{formatAndDivideNumber(upvotes)}</p>
+          <div className="flex-center background-light700_dark400 min-w-[18px] rounded-sm p-1">
+            <p className="subtle-medium text-dark400_light900">{formatAndDivideNumber(upvotes)}</p>
+          </div>
         </div>
 
         <div className="flex-center gap-1.5">
@@ -105,9 +117,10 @@ const Votes = ({ type, itemId, userId, upvotes, hasupVoted, downvotes, hasdownVo
             className="cursor-pointer"
             onClick={() => handleVote("downvote")}
           />
-        </div>
-        <div className="flex-center background-light700_dark400 min-w-[18px] rounded-sm p-1">
-          <p className="subtle-medium text-dark400_light900">{formatAndDivideNumber(downvotes)}</p>
+
+          <div className="flex-center background-light700_dark400 min-w-[18px] rounded-sm p-1">
+            <p className="subtle-medium text-dark400_light900">{formatAndDivideNumber(downvotes)}</p>
+          </div>
         </div>
       </div>
 
